ccb25ff76aef3e937a65d5232024fec0f311f5a2
nurcantemur1 <nurtemur12@gmail.com>
2023-06-15 15:36:36 +0300

    WebApi

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public List<Product> GetProducts() 
+        {
+            return new List<Product>() {
+            new Product { ProductId=1,CategoryId=2,ProductName="g",UnitPrice=8,UnitsInStock=9},
+            new Product {ProductId=1,CategoryId=2,ProductName="f",UnitPrice=8,UnitsInStock=9 }
+            };
+        }
+    }
+}
---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        
+        
+        [HttpGet]
+        public string Get()
+        {
+            /* return new List<Product>
+             {
+                 new Product {ProductId=1, CategoryId = 1,ProductName="ff",UnitPrice=5,UnitsInStock=10},
+                 new Product {ProductId=1, CategoryId = 1,ProductName="ff",UnitPrice=5,UnitsInStock=10 } 
+             };*/
+            return "hello";
+        }
+
+
+    }
+}
